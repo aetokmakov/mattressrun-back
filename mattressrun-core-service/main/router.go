e69a60f257e9bd445b9e46ad26c7852cfa86f5ad
Tokmakov Anton <aetokmakov@domclick.ru>
2023-10-13 19:55:32 +0400

    Added core package and endpoints to get all hotels and hotel by id for v1

---
+++ mattressrun-core-service/main/router.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func initiate() {
+	router := gin.Default()
+
+	router.GET("/hotels", getHotels)
+	router.GET("hotels/:id", getHotelById)
+	router.Run("localhost:9090")
+}
+
+func getHotels(c *gin.Context) {
+	log.Print("Entered getHotels handler")
+
+	c.IndentedJSON(http.StatusOK, hotels)
+
+	log.Print("Finished execution of getHotels handler")
+}
+
+func getHotelById(c *gin.Context) {
+	id := c.Param("id")
+	for _, hotel := range hotels {
+		if hotel.ID == id {
+			c.IndentedJSON(http.StatusOK, hotel)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Hotel not found with passed Id!"})
+}
